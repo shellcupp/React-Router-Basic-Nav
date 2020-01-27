40b3cd78173c38e7ad3cd0814202411bd513f9eb
Restyled.io <commits@restyled.io>
2020-01-27 23:51:00 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
-import { Route } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    <Navigation />
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/contact" component={Contact}/>
-  </div>
-);
+import React from 'react';
+import {Route} from 'react-router-dom';
+
+import {About, Contact, Home, Navigation} from './components';
+
+const App = () =>
+    (<div><Navigation />
+     <Route exact path = "/" component =
+      { Home } />
+    <Route path="/about " component={About} />
+      < Route path = "/contact" component = { Contact } />
+  </div>);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter as Router} from "react-router-dom";
+
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
     <Router>
